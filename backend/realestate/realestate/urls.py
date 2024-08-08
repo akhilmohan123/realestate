ebe8c85da1989cf8ca80a16cd4fddf6547c14670
akhilmohan123 <akhilmohan8547@gmail.com>
2024-08-08 10:32:30 +0530

    Implemented models for user account and jwt

--- backend/realestate/realestate/urls.py
+++ backend/realestate/realestate/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.views.generic import TemplateView
+from django.urls import path,include,re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+    path('api/accounts/',include("accounts.urls"))
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^.* ',TemplateView.as_view(template_name='index.html'))]
